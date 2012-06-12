bf4c42b08d17cfdc671bc2aa9f85ea70bef9893a
Julien Blanchard <julien@sideburns.eu>
2012-06-12 10:50:50 +0200

    add starter kits

--- init.el
+++ init.el
@@ -30,11 +30,17 @@
 (setq ns-command-modifier 'meta)
 (setq ns-function-modifier 'super)
 
+;; Save temp file in /tmp
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; Opens files in the existing frame instead of making new ones.
 (setq ns-pop-up-frames nil)
 
 ;; aesthetics
-(set-frame-font "Menlo-16")
+(set-frame-font "Menlo-14")
 (load-theme 'tango)
 
 ;; package stuff
