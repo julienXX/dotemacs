fd8cf77e411b2287eaf07009719cd8d22e82f28e
Julien Blanchard <julien@sideburns.eu>
2012-06-12 13:45:47 +0200

    remove starter kits, add duplicate function

--- mappings.el
+++ mappings.el
@@ -1,3 +1,4 @@
 (global-set-key "\C-cs" 'shell)
 (global-set-key "\C-cc" 'switch-to-previous-buffer)
-(global-set-key "\M-t" 'peepopen-goto-file-gui)
+(global-set-key "\M-t" 'find-file-in-project)
+(global-set-key "\C-d" 'duplicate-line)
