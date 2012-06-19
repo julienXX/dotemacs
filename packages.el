d5bde2c5e7e5fe70fcc107317a3c914ea304dcea
Julien Blanchard <julien@sideburns.eu>
2012-06-19 11:50:51 +0200

    Added snippets, workaround for ruby-electric messing with YASnippet

--- packages.el
+++ packages.el
@@ -25,6 +25,9 @@
 
 ;; yasnippet
 (require 'yasnippet)
+(require 'yasnippet-bundle)
+(setq yas/root-directory "~/.emacs.d/snippets")
+(yas/load-directory yas/root-directory)
 (yas/global-mode 1)
 
 ;; automatic end insertion
@@ -103,3 +106,15 @@
 
 ;; Ruby-electric
 (require 'ruby-electric)
+
+;; Workaround for ruby-electric messing with yasnippet
+(defun yas/advise-indent-function (function-symbol)
+  (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
+           ,(format
+             "Try to expand a snippet before point, then call `%s' as usual"
+             function-symbol)
+           (let ((yas/fallback-behavior nil))
+             (unless (and (interactive-p)
+                          (yas/expand))
+               ad-do-it)))))
+(yas/advise-indent-function 'ruby-indent-line)
