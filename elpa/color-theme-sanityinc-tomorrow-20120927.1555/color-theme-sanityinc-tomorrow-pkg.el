a75dc1bce8b62fd0a2809737d2dc009c834b6e08
Julien Blanchard <julien@sideburns.eu>
2012-10-01 13:58:47 +0200

    remove obsolete packages

--- elpa/color-theme-sanityinc-tomorrow-20120927.1555/color-theme-sanityinc-tomorrow-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "color-theme-sanityinc-tomorrow" "20120927.1555" "A version of Chris Kempson's various Tomorrow themes [source: github]" 'nil)
-
--- elpa/ctags-update-20120922.1220/ctags-update-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ctags-update" "20120922.1220" "(auto) update TAGS in parent directory using exuberant-ctags [source: github]" (quote nil))
