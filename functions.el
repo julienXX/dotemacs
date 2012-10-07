5b1bce71535efc89229bca56767480258a0d6bce
Julien Blanchard <julien@sideburns.eu>
2012-10-07 12:34:33 +0200

    Add ruby-open-spec-other-buffer function

--- functions.el
+++ functions.el
@@ -111,3 +111,17 @@ Ignores CHAR at point."
                        (search-forward (char-to-string char) nil nil arg)
                      (backward-char direction))
                    (point)))))
+
+(defun ruby-open-spec-other-buffer ()
+  (interactive)
+  (when (featurep 'rspec-mode)
+    (let ((source-buffer (current-buffer))
+          (other-buffer (progn
+                          (rspec-toggle-spec-and-target)
+                          (current-buffer))))
+      (switch-to-buffer source-buffer)
+      (pop-to-buffer other-buffer))))
+
+(eval-after-load 'ruby-mode
+  '(progn
+     (define-key ruby-mode-map (kbd "C-c , ,") 'ruby-open-spec-other-buffer)))
