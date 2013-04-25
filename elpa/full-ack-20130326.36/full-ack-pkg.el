ca072790698b2819b0180f277f4f7944eb7dcfbc
Julien Blanchard <julien@sideburns.eu>
2013-04-25 10:45:19 +0200

    remove old packages, M-t calls helm-mini

--- elpa/full-ack-20130326.36/full-ack-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "full-ack" "20130326.36" "a front-end for ack [source: github]" 'nil)
-
--- elpa/git-gutter-20130416.1345/git-gutter-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-gutter" "20130416.1345" "Port of Sublime Text plugin GitGutter [source: github]" (quote nil))
