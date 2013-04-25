17d9a816de38b095e2637e0124b62ad51469376c
Julien Blanchard <julien@sideburns.eu>
2013-04-25 16:30:40 +0200

    Helm customizations

--- init.el
+++ init.el
@@ -119,4 +119,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(helm-candidate-number ((t (:background "gainsboro" :foreground "black"))))
+ '(helm-selection ((t (:background "yellow")))))
