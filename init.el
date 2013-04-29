9c9cdb6eb9f40efaba2f5b3709dcb19f5910bf23
Julien Blanchard <julien@sideburns.eu>
2013-04-29 10:25:19 +0200

    Better color for erc timestamps

--- init.el
+++ init.el
@@ -119,5 +119,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-timestamp-face ((t (:foreground "gainsboro" :weight bold))))
  '(helm-candidate-number ((t (:background "gainsboro" :foreground "black"))))
  '(helm-selection ((t (:background "yellow")))))
