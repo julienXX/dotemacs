4d0fe9f0dc67dc4e7e38f6430f067c35d1379e7c
Julien Blanchard <julien@sideburns.eu>
2013-05-08 18:30:12 +0200

    Update erc-settings

--- erc-settings.el
+++ erc-settings.el
@@ -2,10 +2,9 @@
 (require 'erc-image)
 
 (setq erc-autojoin-channels-alist
-          '(("freenode.net" "##ability" "#rubyonrails.fr")
-            ("localhost" "#dev" "#the_tigerlily_lobby")))
+          '(("freenode.net" "##ability" "#rubyonrails.fr")))
 
 (erc :server "irc.freenode.net" :port 6667 :nick "julienXX")
-;; (erc :server "localhost" :port 6667 :nick "Julien_Blanchard")
+(erc :server "localhost" :port 6667 :nick "Julien_Blanchard")
 
-(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
