2b37d63b5d6a042922aa619b4b2d925f39752a8d
Julien Blanchard <julien@sideburns.eu>
2013-08-22 15:07:36 +0200

    Add haskell-mode, updates

---
+++ elpa/ghc-20130520.1540/ghc-pkg.el
@@ -0,0 +1,2 @@
+(define-package "ghc" "20130520.1540" "Sub mode for Haskell mode [github]" 'nil)
+
---
+++ elpa/haskell-mode-20130819.1523/haskell-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "haskell-mode" "20130819.1523" "A Haskell editing mode [github]" 'nil)
+
