3fe2108a437a654c88be008cd103cac2e75e6355
Julien Blanchard <julien@sideburns.eu>
2013-08-25 15:47:22 +0200

    Add a new line before git commentary, close popup when commiting

--- setup-magit.el
+++ setup-magit.el
@@ -63,5 +63,17 @@
 
 (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
+;; Add an extra newline to separate commit message from git commentary
+(defun magit-commit-mode-init ()
+  (when (looking-at "\n")
+    (open-line 1)))
+
+(add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
+
+;; close popup when commiting
+
+(defadvice git-commit-commit (after delete-window activate)
+  (delete-window))
+
 
 (provide 'setup-magit)
