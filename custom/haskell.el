78cbcbf2e7caa91c25a019e0db0707c2ff3f4bdd
Julien Blanchard <julien@sideburns.eu>
2014-11-10 17:37:16 +0100

    Add my-cabal-path

--- custom/haskell.el
+++ custom/haskell.el
@@ -1,3 +1,7 @@
+(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+  (add-to-list 'exec-path my-cabal-path))
+
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
