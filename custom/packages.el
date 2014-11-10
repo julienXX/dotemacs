61ac564abbe8ed24570256184152c38d1f6d5a5e
Julien Blanchard <julien@sideburns.eu>
2014-11-10 17:35:57 +0100

    Remove grizzle

--- custom/packages.el
+++ custom/packages.el
@@ -146,10 +146,8 @@
 (setq git-gutter:deleted-sign "-- ")
 
 ;; projectile
-(require 'grizzl)
 (projectile-global-mode)
 (setq projectile-enable-caching t)
-(setq projectile-completion-system 'grizzl)
 ;; Press Command-p for fuzzy find in project
 (global-set-key (kbd "M-p") 'projectile-find-file)
 ;; Press Command-b for fuzzy switch buffer
