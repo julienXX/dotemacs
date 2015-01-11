2b62d23abdfe43291e50e7a95bec8c9ae71ed8f1
Julien Blanchard <julien@sideburns.eu>
2015-01-11 16:06:42 +0100

    Use helm-M-x

--- custom/mappings.el
+++ custom/mappings.el
@@ -1,3 +1,4 @@
+(global-set-key (kbd "M-x")          'helm-M-x)
 (global-set-key (kbd "C-c s")        'shell)
 (global-set-key (kbd "C-x C-v")      'ido-find-file-other-window)
 (global-set-key (kbd "C-c c")        'switch-to-previous-buffer)
