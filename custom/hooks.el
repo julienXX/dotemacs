f95519ed979ff6c25f9b0aedbaf761e5b9a0305d
Julien Blanchard <julien@sideburns.eu>
2015-02-23 11:55:18 +0100

    remove company mode

--- custom/hooks.el
+++ custom/hooks.el
@@ -69,9 +69,3 @@
 
 ;; Clojure
 (add-hook 'clojure-mode-hook 'paredit-mode)
-
-;; Company mode
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-to-list 'company-backends 'company-ghc)
-(custom-set-variables '(company-ghc-show-info t))
