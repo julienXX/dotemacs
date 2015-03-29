47d57b60ef030e3ae87f2e1972afc9adc8348ff9
Julien Blanchard <julien@sideburns.eu>
2015-03-29 18:07:42 +0200

    init.el doc

--- init.el
+++ init.el
@@ -1,4 +1,28 @@
-;; emacs configuration
+;;; -*- lexical-binding: t -*-
+;;; init.el --- Emacs main config
+
+;; Copyright (C) 2015 Julien Blanchard
+
+;; Author: Julien Blanchard <julien@sideburns.eu>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
 ;; Setting path
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path `("/usr/local/bin")))
@@ -153,3 +177,7 @@
  '(helm-selection ((t (:background "#dd6767" :foreground "#eeeeee"))))
  '(helm-source-header ((t (:background "#A9C5C3" :foreground "black" :weight bold :height 1.3 :family "Sans Serif"))))
  '(show-paren-match ((t (:background "yellow")))))
+
+
+(provide 'init)
+;;; init.el ends here
