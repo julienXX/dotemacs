304cced1e6aeba403d8ad4f716424869d50495a9
Julien Blanchard <julien@sideburns.eu>
2016-06-06 14:58:54 +0200

    Much simpler haskell config with intero

--- modules/jxx-haskell.el
+++ modules/jxx-haskell.el
@@ -21,40 +21,7 @@
 
 (require 'haskell-mode)
 
-;; Setup haskell-mode hooks
-(with-eval-after-load "haskell-mode"
-  (custom-set-variables
-   '(haskell-mode-hook
-     '(turn-on-haskell-indentation
-       turn-on-haskell-doc))))
-
-(add-hook 'haskell-mode-hook
-          (lambda () (electric-indent-local-mode 0)))
-
-;; Setup haskell-interactive-mode keybindings. Get started by using C-c C-z from
-;; a buffer visiting a file in your Haskell project.
-(with-eval-after-load "haskell-mode"
-  ;; Switch to the current REPL buffer, starting a session if needed.
-  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  ;; Switch between REPL sessions.
-  (define-key haskell-mode-map (kbd "C-c b") 'haskell-session-change)
-  ;; Load the current buffer into the REPL.
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-  ;; Infer the type of the thing at point.
-  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-  ;; Display info (in the REPL) about the thing at point.
-  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-  ;; Insert the inferred type of the function at point into the code.
-  (define-key haskell-mode-map (kbd "C-c C-s") (lambda () (interactive) (haskell-process-do-type t)))
-  ;; Run `cabal test' in a compile buffer.
-  (define-key haskell-mode-map (kbd "C-c C-,") 'jxx-haskell/run-test-suite))
-
-;; A function for launching a compile buffer with `cabal test'.
-(defun jxx-haskell/run-test-suite ()
-  (interactive)
-  (require 'compile)
-  (projectile-with-default-dir (projectile-project-root)
-    (compile "cabal test")))
+(add-hook 'haskell-mode-hook 'intero-mode)
 
 (provide 'jxx-haskell)
 ;;; jxx-haskell.el ends here
