4887b906918a159ea36026122c5824c9cd38d0f1
Julien Blanchard <julien@sideburns.eu>
2018-05-22 11:02:49 +0200

    Elixir module

--- modules/jxx-hooks.el
+++ modules/jxx-hooks.el
@@ -67,14 +67,6 @@
 ;; FlyCheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;; Elixir
-(add-hook 'elixir-mode-hook
-          (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-            (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                 "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-            (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-            (ruby-end-mode +1)))
-
 
 (provide 'jxx-hooks)
 ;;; jxx-hooks.el ends here
