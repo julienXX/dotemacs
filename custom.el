2f45acaa630e9999685d8b60ef3a014287b9abff
Julien Blanchard <julien@typed-hole.org>
2022-03-18 10:18:32 +0100

    Tune some settings

--- custom.el
+++ custom.el
@@ -4,10 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elfeed-protocol mu4e-marker-icons mu4e-column-faces github-browse-file markdown-mode flycheck xref ivy-xref delight flycheck-rust cargo racer dumb-jump csharp-mode fsharp-mode dotnet cider company counsel elpher ghub code-review doom-modeline org-superstar origami smart-mode-line diminish deft emacsql-sqlite3 org-roam elfeed smart-compile ivy-hydra typescript-mode mu4e-alert mu4e terraform-mode go-mode go restclient uncrustify-mode ruby-mode sicp plantuml-mode rust-mode yasnippet yaml-mode wrap-region use-package unkillable-scratch soft-morning-theme smex smartparens ruby-tools rspec-mode popwin org multi-term magit lsp-mode js2-mode golden-ratio git-gutter gist flycheck-credo flx-ido expand-region diredfl dired-git-info counsel-projectile anzu all-the-icons-ivy alchemist ace-jump-mode)))
+   '(code-review counsel-at-point lsp-haskell haskell-mode json-mode pocket-reader elfeed-protocol mu4e-marker-icons mu4e-column-faces github-browse-file markdown-mode flycheck xref ivy-xref delight flycheck-rust cargo racer dumb-jump csharp-mode fsharp-mode dotnet cider company counsel elpher ghub doom-modeline org-superstar origami smart-mode-line diminish deft emacsql-sqlite3 org-roam elfeed smart-compile ivy-hydra typescript-mode mu4e-alert mu4e terraform-mode go-mode go restclient uncrustify-mode ruby-mode sicp plantuml-mode rust-mode yasnippet yaml-mode wrap-region use-package unkillable-scratch soft-morning-theme smex smartparens ruby-tools rspec-mode popwin org multi-term magit lsp-mode js2-mode golden-ratio git-gutter gist flycheck-credo flx-ido expand-region diredfl dired-git-info counsel-projectile anzu all-the-icons-ivy alchemist ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(gnus-header-content ((t (:foreground "cornflower blue")))))
+ '(gnus-header-content ((t (:foreground "light coral")))))
