b4d52b8815c9e666f58820d218d83a5f605f8240
Julien Blanchard <julien@typed-hole.org>
2022-08-01 10:04:01 +0200

    Update GNUS, rust, LSP

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(canlock-password "acbce107ee2731c54d17fd37f648183ba073ca9e")
+ '(custom-safe-themes
+   '("8efa3d21b3fa1ac084798fae4e89848ec26ae5c724b9417caf4922f4b2e31c2a" "c5a6e48f90c38a78f2f4adf446acf6cea36c8aa8b0a59058e6c4d08a800a3d0e" default))
  '(package-selected-packages
    '(zeal-at-point detached srht org-gcal dockerfile-mode code-review counsel-at-point lsp-haskell haskell-mode json-mode pocket-reader elfeed-protocol mu4e-marker-icons mu4e-column-faces github-browse-file markdown-mode flycheck xref ivy-xref delight flycheck-rust cargo racer dumb-jump csharp-mode fsharp-mode dotnet cider company counsel elpher ghub doom-modeline org-superstar origami smart-mode-line diminish deft emacsql-sqlite3 org-roam elfeed smart-compile ivy-hydra typescript-mode mu4e-alert mu4e terraform-mode go-mode go restclient uncrustify-mode ruby-mode sicp plantuml-mode rust-mode yasnippet yaml-mode wrap-region use-package unkillable-scratch soft-morning-theme smex smartparens ruby-tools rspec-mode popwin org multi-term magit lsp-mode js2-mode golden-ratio git-gutter gist flycheck-credo flx-ido expand-region diredfl dired-git-info counsel-projectile anzu all-the-icons-ivy alchemist ace-jump-mode))
  '(smtpmail-smtp-server "mail.typed-hole.org")
