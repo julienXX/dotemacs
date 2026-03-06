cbf4a80da934447d07b501f2eca74e6758702ca6
Julien Blanchard <julien@typed-hole.org>
2026-03-06 17:33:37 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -5,10 +5,33 @@
  ;; If there is more than one, they won't work right.
  '(canlock-password "2a9b439ac3a993b3c39916308e7b69fdca5357dc")
  '(custom-safe-themes
-   '("835d934a930142d408a50b27ed371ba3a9c5a30286297743b0d488e94b225c5f" "96005f97499f0549f921f81588f190f189b7acb8bbebbcbb9033cdd340118f80" "f56e81765ccd0ee403860bd1d0a2f9967aa132b4a6f40517dd5eb13f7726eaba" default))
+   '("92c6de0850bb66a5e917de9c1e80f0baa0e5b88e74b2c2cd1e25f875a69de7a7"
+     "835d934a930142d408a50b27ed371ba3a9c5a30286297743b0d488e94b225c5f"
+     "96005f97499f0549f921f81588f190f189b7acb8bbebbcbb9033cdd340118f80"
+     "f56e81765ccd0ee403860bd1d0a2f9967aa132b4a6f40517dd5eb13f7726eaba"
+     default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(acme-theme grayscale-theme focus nerd-icons alert bundler octicons editorconfig sharper chatgpt-shell undo-tree which-key ivy-prescient prescient crux org-caldav oauth2 wgrep simplecov tree-sitter-langs tree-sitter clang-format eglot handlebars-mode rubocop rustic sideline-flymake mastodon grey-paper-theme sideline-lsp lsp-ui sideline sideline-flycheck mu4e-marker-icons forge twtxt jsonian zeal-at-point detached srht org-gcal dockerfile-mode code-review counsel-at-point lsp-haskell haskell-mode json-mode pocket-reader elfeed-protocol mu4e-column-faces github-browse-file markdown-mode flycheck xref ivy-xref delight flycheck-rust cargo racer dumb-jump csharp-mode fsharp-mode dotnet cider company counsel elpher ghub doom-modeline org-superstar origami smart-mode-line diminish deft emacsql-sqlite3 org-roam elfeed smart-compile ivy-hydra typescript-mode mu4e-alert mu4e terraform-mode go-mode go restclient uncrustify-mode ruby-mode sicp plantuml-mode rust-mode yasnippet yaml-mode wrap-region use-package unkillable-scratch soft-morning-theme smex smartparens ruby-tools rspec-mode popwin org multi-term magit lsp-mode js2-mode golden-ratio git-gutter gist flycheck-credo flx-ido expand-region diredfl dired-git-info counsel-projectile anzu all-the-icons-ivy alchemist ace-jump-mode))
+   '(ace-jump-mode agent-shell ai-code all-the-icons-ivy anzu
+                   backward-forward bazel cape cargo citre claude-code
+                   company consult-eglot copilot corfu-prescient
+                   counsel-projectile crux dap-mode dape dashboard
+                   deft delight devcontainer devdocs diminish
+                   directory-slideshow dired-git-info diredfl
+                   doom-modeline dumb-jump elfeed-protocol ellama
+                   elpher emojify exec-path-from-shell expand-region
+                   flycheck-rust flymake-racket focus gist git-gutter
+                   golden-ratio ivy-hydra ivy-lobsters ivy-prescient
+                   js2-mode lorem-ipsum lsp-ivy lsp-ui magit mastodon
+                   mathjax minions minuet moody mu4e-alert
+                   mu4e-column-faces multi-term nano-modeline
+                   org-download org-present org-roam org-superstar
+                   pgmacs popwin protobuf-ts-mode racket-mode rbenv
+                   robe rspec-mode ruby-tools rustic smartparens smex
+                   soft-morning-theme solaire-mode svelte-mode svg-lib
+                   tempel tree-sitter treesit-auto typescript-mode
+                   undo-tree unkillable-scratch vue-mode websocket
+                   wrap-region yaml-mode yasnippet))
  '(smtpmail-smtp-server "mail.typed-hole.org")
  '(smtpmail-smtp-service 25))
 (custom-set-faces
